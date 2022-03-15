f04984ae203afe574c5b624e8c7747a7a2290339
lrovaris <luis.gabrielrovaris@gmail.com>
2022-03-15 13:54:53 -0300

    asd

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -149,7 +149,7 @@ public class GameManager : MonoBehaviour
 
     private void HandleGameOver()
     {
-        netScript.instance.MakeTransfer((int)GetPot(), Wallet)
+        netScript.instance.MakeTransfer((int) GetPot(), Wallet);
         Debug.Log("GameOver");
         GameEvents.instance.MatchEnd();
     }
