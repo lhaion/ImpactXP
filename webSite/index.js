e206c33451e6334d325fc582d3a50efc5b21ddd0
lrovaris <luis.gabrielrovaris@gmail.com>
2022-05-19 19:07:10 -0300

    Mint Website updated (working)

---
+++ webSite/index.js
@@ -0,0 +1,8 @@
+import * as web3 from 'https://cdn.jsdelivr.net/gh/ethereum/web3.js/dist/web3.min.js';
+
+function log(shit) {
+
+    console.log(web3)
+    console.log(shit);
+    console.log(window.ethereum)
+}
